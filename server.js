fc608a019a958ff18d21e5134414080ba993854a
Kirito <devavratk96@gmail.com>
2020-07-08 23:45:16 -0400

    Working server

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-// New code here
+const path = require('path');
+const express = require('express');
+const port = process.env.port || 3000;
+
+const app = express();
+
+app.listen(port, () => {
+    console.log("Server is running on port 3000.")
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+})
+
+app.post('/fileUpload', (req, res) => {
+    console.log(req.body);
+    console.log(req.params);
+})
