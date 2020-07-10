a578fdf3920a3a5d6f204765b4a405c1fe30fb87
Kirito <devavratk96@gmail.com>
2020-07-10 02:48:25 -0400

    added Procfile

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 const port = process.env.port || 3000;
 app.listen(port, () => {
-    console.log("Server is running on port 3000.")
+    console.log("Server is running on port " + port);
 })
 
 app.use(express.static('public'));
